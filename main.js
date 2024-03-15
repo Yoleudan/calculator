ba996439b1c1b7496c5bf1c9f1425a275708c092
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-03-15 14:31:44 +0000

    Add operate function

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let num1 = 0
-let num2 = 0
-let operator = 0
+let userNumber1 = 6
+let userNumber2 = 3
+let userOperator = 0
 
 function add(num1, num2){
     return num1 + num2;
 }
 
 function subtract(num1, num2){
-    return num1 + num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2){
@@ -16,4 +16,23 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return num1 / num2;
+}
+
+function operate(){
+    if (userOperator === "+"){
+        return add(userNumber1, userNumber2);
+    }
+    else if(userOperator === "-"){
+        return subtract(userNumber1, userNumber2);
+    }
+    else if(userOperator === "*"){
+        return multiply(userNumber1, userNumber2);
+    }
+    else if(userOperator === "/"){
+        return divide(userNumber1, userNumber2);
+    }
+    else{
+        return alert("error")
+    }
+    
 }
