ff0d0212de7405b0a391852d6ba8084bed8e0ced
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-03-16 15:49:18 +0000

    Program buttons of calc

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let userNumber1 = 6
-let userNumber2 = 3
-let userOperator = 0
+let userNumber1 = ""
+let userNumber2 = ""
+let userOperator = ""
+let display = 0
+let result = 0
 
 function add(num1, num2){
     return num1 + num2;
@@ -36,3 +38,104 @@ function operate(){
     }
     
 }
+
+function updateDisplay(){
+    display = userNumber1 + " " + userOperator + " " + userNumber2
+    return content.textContent = (display);
+}
+
+function appendOrReplace(input){
+    if (userNumber1 === 0){
+        userNumber1 = input.toString();
+    }
+    else{
+        userNumber1 += input.toString();
+    }
+
+}
+
+const disp = document.querySelector('#display')
+
+const content = document.createElement('div')
+content.classList.add('content')
+content.textContent = (display)
+disp.appendChild(content);
+
+
+
+const one = document.querySelector('#one')
+one.addEventListener('click', () =>{
+    appendOrReplace(1);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const two = document.querySelector('#two')
+two.addEventListener('click', () =>{
+    appendOrReplace(2);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const three = document.querySelector('#three')
+three.addEventListener('click', () =>{
+    appendOrReplace(3);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const four = document.querySelector('#four')
+four.addEventListener('click', () =>{
+    appendOrReplace(4);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const five = document.querySelector('#five')
+six.addEventListener('click', () =>{
+    appendOrReplace(6);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const seven = document.querySelector('#seven')
+seven.addEventListener('click', () =>{
+    appendOrReplace(7);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const eight = document.querySelector('#eight')
+eight.addEventListener('click', () =>{
+    appendOrReplace(8);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const nine = document.querySelector('#nine')
+nine.addEventListener('click', () =>{
+    appendOrReplace(9);
+    updateDisplay();
+    console.log(userNumber1)
+})
+const zero = document.querySelector('#zero')
+zero.addEventListener('click', () =>{
+    appendOrReplace(0);
+    updateDisplay();
+    console.log(userNumber1)
+})
+
+
+const divideBtn = document.querySelector('#divide')
+divideBtn.addEventListener('click', () =>{
+    userOperator = "÷";
+    updateDisplay();
+})
+const addBtn = document.querySelector('#add')
+addBtn.addEventListener('click', () =>{
+    userOperator = "+";
+    updateDisplay();
+})
+const minusBtn = document.querySelector('#minus')
+minusBtn.addEventListener('click', () =>{
+    userOperator = "-";
+    updateDisplay();
+})
+const multiplyBtn = document.querySelector('#multiply')
+multiplyBtn.addEventListener('click', () =>{
+    userOperator = "*";
+    updateDisplay();
+})
